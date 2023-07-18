ede3127da0dad2899c02e93cf35c804978a34a92
Sasha Petrov <petrov.evpanet@gmail.com>
2023-07-18 08:58:14 +0100

    Added langchain support

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ import os
 import openai
 from dotenv import load_dotenv
 
+#langchain imports
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -14,7 +23,7 @@ cors = CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Load the OpenAI API key from an environment variable
-openai.api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route("/api/improve_text", methods=["POST"])
 def improve_text():
@@ -28,19 +37,17 @@ def improve_text():
         "blog": "I want you to act as an editor, spelling corrector, and grammar improver. I’m writing a blog post for Substack or an article for Medium. I want to sound more like a native speaker but keep it simple and avoid words and phrases that may sound too pretentious. I will send you a sentence or paragraph, and you’ll send me the result. Reply only with the improved text and nothing else, do not write explanations.",
     }
 
+
     messages = [
-        {"role": "system", "content": prompts[selected_prompt]},
-        {"role": "user", "content": input_text},
+        SystemMessage(content=prompts[selected_prompt]),
+        HumanMessage(content=input_text)
     ]
 
-    payload = {
-        "model": "gpt-3.5-turbo",
-        "messages": messages,
-    }
+    llm = ChatOpenAI(temperature=0.5, model_name='gpt-3.5-turbo')
 
     try:
-        response = openai.ChatCompletion.create(**payload)
-        return jsonify(response)
+        result = llm(messages)
+        return jsonify(result.content)
     except Exception as e:
         print(e)
         return jsonify({"error": "Unable to fetch results"}), 500
